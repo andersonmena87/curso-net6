8888ba124351510c5022e03207ceff3bbe692e76
Anderson Mena <amserna@stefanini.com>
2023-05-29 20:43:07 -0500

    Actualización

---
+++ ejemplo-consola-net-framework/EjemploConsolaNetFramework/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EjemploConsolaNetFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mensaje = "Hola mundo!";
+            Console.WriteLine(mensaje);
+
+            mensaje = Console.ReadLine();
+            Console.WriteLine(mensaje);
+
+
+        }
+    }
+}
